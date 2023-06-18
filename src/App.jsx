a0d3a7313dac7f7f23f3843a4ce19392b565ad53
Ritesh <riteshdas8652@gmail.com>
2023-06-18 10:01:04 +0530

    Scaffolding the Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+// import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/signin">SignIn</Route>
-        <Route path="/">Home</Route>
-      </Switch>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Switch>
+    //     <Route path="/signin">SignIn</Route>
+    //     <Route path="/">Home</Route>
+    //   </Switch>
+    // </BrowserRouter>
+    <div>hello</div>
   );
 }
 

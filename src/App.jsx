c1ee87b86709cb0dac09980ea6615f20fca42abc
Ritesh <riteshdas8652@gmail.com>
2023-06-22 20:39:45 +0530

    Creating Dashboard 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Switch } from 'react-router';
+import React from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
 import SignIn from './pages/SignIn';
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,4 +1,4 @@
-// import React, { useContext } from 'react';
+import React from 'react';
 import { Redirect, Route } from 'react-router';
 import { useProfile } from '../context/profile.context';
 import { Container, Loader } from 'rsuite';

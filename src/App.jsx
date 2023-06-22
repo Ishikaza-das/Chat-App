a436e2914501fd400edba98633176c264b6103f9
Ritesh <riteshdas8652@gmail.com>
2023-06-22 10:55:08 +0530

    Creating Pages - Private & Public Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-// import { Switch, Route, BrowserRouter } from 'react-router-dom';
-
+import { Switch } from 'react-router-dom';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import SignIn from './pages/SignIn';
+import Home from './pages/Home';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Switch>
-    //     <Route path="/signin">SignIn</Route>
-    //     <Route path="/">Home</Route>
-    //   </Switch>
-    // </BrowserRouter>
-    <div>hello</div>
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
+}
+
+export default App;

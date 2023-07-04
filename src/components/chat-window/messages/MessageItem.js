1a4d5e77da50b9f3f1eecee0a8ee6aa01c485761
Ritesh <riteshdas8652@gmail.com>
2023-07-04 19:10:51 +0530

    Display User Profile Data

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProfileAvatar from '../../ProfileAvatar';
 import TimeAgo from 'timeago-react';
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 const MessageItem = ({ messages }) => {
   const { author, crreatedAt, text } = messages;
@@ -15,7 +16,11 @@ const MessageItem = ({ messages }) => {
           size="xs"
         />
 
-        <span className="ml-2">{author.name}</span>
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        />
         <TimeAgo
           datetime={crreatedAt}
           className="font-normal text-black-45 ml-2"
